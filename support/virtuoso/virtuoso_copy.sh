f8bbdfef2f9ebb191c5b54c07e91d31c8d5c9d5c
Vincent Emonet <vincent.emonet@gmail.com>
2019-10-15 00:09:54 +0200

    make sure load.sh is executable

--- support/virtuoso/virtuoso_copy.sh
+++ support/virtuoso/virtuoso_copy.sh
@@ -3,7 +3,9 @@
 # Copy the RDF output file from xml2rdf or r2rml to the shared dir
 cp rdf_output.nq /data/red-kg/
 
-# Copy virtuoso load script to shared dir
+# Copy virtuoso load.sh script to shared dir
 cp $1 /data/red-kg
+
+chmod +x /data/red-kg/load.sh
 
 # cp $1 /data/red-kg/
