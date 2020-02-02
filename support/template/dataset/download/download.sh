79f0bb12e2c3fc015ad28c323cf361891265caf1
Vincent Emonet <vincent.emonet@gmail.com>
2020-02-02 19:26:37 +0100

    now use mapping/ as directory in datasets instead of mappings/

--- support/template/dataset/download/download.sh
+++ support/template/dataset/download/download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-## Download files
+## Download example TSV files from GitHub (COHD clinical data)
 
 wget -N https://raw.githubusercontent.com/MaastrichtU-IDS/d2s-transform-template/master/datasets/cohd/download/cohd-sample/concepts.tsv
 wget -N https://raw.githubusercontent.com/MaastrichtU-IDS/d2s-transform-template/master/datasets/cohd/download/cohd-sample/paired_concept_counts_associations.tsv
