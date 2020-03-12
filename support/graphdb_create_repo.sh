5f3fefe9239e3960aeb141192592d3227f3f0236
Vincent Emonet <vincent.emonet@gmail.com>
2020-03-12 20:02:35 +0100

    use tmp-virtuoso instead of virtuoso-tmp, now use graphdb as final triplestore and change repo to demo (was test)

---
+++ support/graphdb_create_repo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# TODO: add a parameters to define the triplestore URL in d2s-bash-exec args
+# echo if command failed (we don't want to stop the workflow for this)
+curl -X POST http://graphdb:7200/rest/repositories -H 'Content-Type: multipart/form-data' -F "config=@d2s-cwl-workflows/support/graphdb-repo-config.ttl" || echo "cURL to create GraphDB test repository failed."
