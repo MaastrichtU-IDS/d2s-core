a0def831632f1f2591aafada481b947710c7477f
Vincent Emonet <vincent.emonet@gmail.com>
2020-03-16 22:31:40 +0100

    fix template rml mappings

--- support/template/dataset/download/download.sh
+++ support/template/dataset/download/download.sh
@@ -8,7 +8,7 @@ wget -N https://github.com/MaastrichtU-IDS/d2s-scripts-repository/raw/master/res
 wget -N https://github.com/MaastrichtU-IDS/d2s-scripts-repository/raw/master/resources/drugbank-sample/drugbank.xml
 
 # Download JSON to test RML
-https://github.com/MaastrichtU-IDS/d2s-scripts-repository/raw/master/resources/rml-test.json
+wget -N https://github.com/MaastrichtU-IDS/d2s-scripts-repository/raw/master/resources/rml-test.json
 
 # Convert TSV to CSV for RML
 sed -e 's/"/\\"/g' -e 's/\t/","/g' -e 's/^/"/' -e 's/$/"/'  -e 's/\r//' paired_concept_counts_associations.tsv > paired_concept_counts_associations.csv
